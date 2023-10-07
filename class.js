8235a04bcaa014d3e1deb58aa86750fcb4b41911
nareeeee <nare.unusyan@tumo.org>
2023-10-07 16:52:38 +0400

    Update 3.7.5.4

--- class.js
+++ class.js
@@ -1,33 +1,6 @@
-class Grass{
-    constructor(x, y) {
-       this.x = x;
-       this.y = y;
-       this.multiply = 0;
-       this.directions = [
-        [this.x - 1, this.y - 1],
-        [this.x    , this.y - 1],
-        [this.x + 1, this.y - 1],
-        [this.x - 1, this.y    ],
-        [this.x + 1, this.y    ],
-        [this.x - 1, this.y + 1],
-        [this.x    , this.y + 1],
-        [this.x + 1, this.y + 1]
-    ];
+class Grass extends LivingCreature{
 
-    }
-    chooseCell(character) {
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
-            if (matrix[y][x] == character) {
-                found.push(this.directions[i]);
-            }
-        }
-    }
-        return found;
-     }
+
      mul(){
         let newCell = random(this.chooseCell(0));
         if(newCell && this.multiply >= 1){
@@ -43,12 +16,13 @@ class Grass{
 }
 
 
-class GrassEater{
+
+
+class GrassEater extends LivingCreature{
     constructor(x,y){
-        this.x = x;
-        this.y = y;
+        super(x, y);
         this.energy = 3;
-        this.directions = [];
+
     }
 
     getNewCoordinates(){
@@ -63,19 +37,12 @@ class GrassEater{
             [this.x + 1, this.y + 1]
         ];
     }
+
+
     chooseCell(character){
         this.getNewCoordinates();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
-            if (matrix[y][x] == character) {
-                found.push(this.directions[i]);
-            }
-        }
-    }
-    return found;
+        return super.chooseCell(character);
+    
 }
 
 mul(){
--- monster.js
+++ monster.js
@@ -1,9 +1,8 @@
-class Monster{
+class Monster extends LivingCreature{
     constructor(x,y){
-        this.x = x;
-        this.y = y;
+        super(x, y);
         this.energy = 3;
-        this.directions = [];
+     
     }
 
     getNewCoordinates(){
@@ -18,19 +17,11 @@ class Monster{
             [this.x + 1, this.y + 1]
         ];
     }
+    
     chooseCell(character){
         this.getNewCoordinates();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
-            if (matrix[y][x] == character) {
-                found.push(this.directions[i]);
-            }
-        }
-    }
-    return found;
+        return super.chooseCell(character);
+    
 }
 
 mul(){
